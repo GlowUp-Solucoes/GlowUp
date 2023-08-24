98935f0f29b466f2158a45ecf8b2f65bce86c2f8
Christian Lima <christiandasilvalima@hotmail.com>
2023-08-24 02:06:26 -0300

    feat form and send whatsapp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Form from '@/components/form'
 
 export default function Home() {
+
   return (
     <main className={styles.main}>
-      
+      <Form />
     </main>
   )
 }
